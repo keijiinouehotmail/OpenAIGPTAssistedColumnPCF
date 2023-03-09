a04a257ca4a53e0f8a5967673b1d77b1a4c88a7c
keijiinouehotmail <Keiji.Inoue@hotmail.com>
2023-03-09 16:39:55 +0900

    Added versionCheck.ps1 for "prebuild" npm script

--- OpenAIGPTAssistedColumn/index.ts
+++ OpenAIGPTAssistedColumn/index.ts
@@ -3,7 +3,7 @@ import * as React from "react";
 import { AssistedColumnReact, IAssistedColumnReactProps } from "./AssistedColumnReact";
 
 export class OpenAIGPTAssistedColumn implements ComponentFramework.ReactControl<IInputs, IOutputs> {
-    private version = "1.0.4";
+    private version = '1.0.4';
     private theComponent: ComponentFramework.ReactControl<IInputs, IOutputs>;
     private notifyOutputChanged: () => void;
     private _value: string | undefined;
---
+++ versionCheck.ps1
@@ -0,0 +1,21 @@
+echo 'Checking version string in several files for OpenAIGPTAssistedColumnPCF'
+$SolutionVersion = ((Get-Content .\OpenAIGPTAssistedColumn\Solutions\src\Other\Solution.xml | Select-String -Pattern '<Version>') -replace '^ *<Version>', '') -replace '[^0-9\.]', ''
+$message = @('-> Solution Version', $SolutionVersion) -join ' = '
+echo $message
+
+$ManifestVersion = ((Get-Content .\OpenAIGPTAssistedColumn\ControlManifest.Input.xml | Select-String -Pattern 'version="')[1] -replace '^ *version="', '') -replace '[^0-9\.]', ''
+$message = @('-> Manifest Version', $ManifestVersion) -join ' = '
+echo $message
+
+$VersionInCode =((Get-Content .\OpenAIGPTAssistedColumn\index.ts | Select-String -Pattern 'version *=') -replace '^.*=', '') -replace '[^0-9\.]', ''
+$message = @('-> Version in Code', $VersionInCode) -join '  = '
+echo $message
+
+if(($SolutionVersion -eq $ManifestVersion) -and ($SolutionVersion -eq $VersionInCode)){
+  echo 'Version check succeeded'
+  exit 0
+}else{
+  echo 'Version check failed'
+  exit -1
+}
+
